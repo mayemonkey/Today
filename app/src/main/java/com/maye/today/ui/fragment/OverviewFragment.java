4f52288e092ee6d386b6efe0647cf32a20ecaf0b
maye <ssdd050@163.com>
2016-11-15 23:28:37 +0800

    修改MonkeyDatePager依赖

--- app/src/main/java/com/maye/today/ui/fragment/OverviewFragment.java
+++ app/src/main/java/com/maye/today/ui/fragment/OverviewFragment.java
@@ -9,21 +9,21 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.maye.today.domain.Record;
-import com.maye.today.global.TodayApplication;
-import com.maye.today.monkeytabpager.MonkeyTabPager;
+
 import com.maye.today.record.RecordPresenter;
 import com.maye.today.record.RecordPresenterImpl;
 import com.maye.today.record.RecordView;
 import com.maye.today.today.R;
 import com.maye.today.ui.adapter.RecordAdapter;
 import com.maye.today.view.LoadListView;
+import com.maye.view.MonkeyDatePager;
 
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
 
-public class OverviewFragment extends Fragment implements MonkeyTabPager.OnMonkeyTimeChangedListener, LoadListView.OnLoadMoreListener, SwipeRefreshLayout.OnRefreshListener, RecordView {
+public class OverviewFragment extends Fragment implements  LoadListView.OnLoadMoreListener, SwipeRefreshLayout.OnRefreshListener, RecordView, MonkeyDatePager.OnMonkeyTimeChangedListener {
 
     private View view;
 
@@ -32,7 +32,7 @@ public class OverviewFragment extends Fragment implements MonkeyTabPager.OnMonke
     private LoadListView llv_overview;
     private RecordPresenter recordPresenter;
 
-    private MonkeyTabPager mtp_time;
+    private MonkeyDatePager mdp_time;
 
 
     private boolean isRefresh = true;
@@ -50,14 +50,9 @@ public class OverviewFragment extends Fragment implements MonkeyTabPager.OnMonke
     }
 
     private void initComponent() {
-        mtp_time = (MonkeyTabPager) view.findViewById(R.id.mtp_time);
-        Calendar time = mtp_time.getTime();
-        List<String> list_date = new ArrayList<>();
-        for (int i = 0; i < time.getActualMaximum(Calendar.DAY_OF_MONTH); i++){
-            list_date.add(String.valueOf(i + 1));
-        }
-        mtp_time.setTypeAndData(MonkeyTabPager.DAY, list_date);
-        mtp_time.setOnMonkeyTimeChangedListener(this);
+        mdp_time = (MonkeyDatePager) view.findViewById(R.id.mdp_time);
+
+        mdp_time.setOnMonkeyTimeChangedListener(this);
 
         srl_over = (SwipeRefreshLayout) view.findViewById(R.id.srl_overview);
         srl_over.setColorSchemeResources(android.R.color.holo_blue_bright, android.R.color.holo_green_light,
@@ -76,19 +71,19 @@ public class OverviewFragment extends Fragment implements MonkeyTabPager.OnMonke
     public void onResume() {
         super.onResume();
         recordPresenter = new RecordPresenterImpl(this);
-        recordPresenter.showRecordByAssignTime("", mtp_time.getType(), "2016-10-10", 0);
+        recordPresenter.showRecordByAssignTime("", mdp_time.getType(), "2016-10-10", 0);
     }
 
     @Override
     public void onRefresh() {
 
-        recordPresenter.showRecordByAssignTime("", mtp_time.getType(), "2011-04-01", 0);
+        recordPresenter.showRecordByAssignTime("", mdp_time.getType(), "2011-04-01", 0);
         isRefresh = true;
     }
 
     @Override
     public void onLoadMore() {
-        recordPresenter.showRecordByAssignTime("username", mtp_time.getType(), "time", start);
+        recordPresenter.showRecordByAssignTime("username", mdp_time.getType(), "time", start);
         isRefresh = false;
     }
 
@@ -119,7 +114,7 @@ public class OverviewFragment extends Fragment implements MonkeyTabPager.OnMonke
     @Override
     public void onTimeChanged(Calendar time) {
         start = 0;
-        recordPresenter.showRecordByAssignTime("username", mtp_time.getType(), "today", start);
+        recordPresenter.showRecordByAssignTime("username", mdp_time.getType(), "today", start);
     }
 
     @Override
