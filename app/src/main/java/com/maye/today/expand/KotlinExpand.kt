ddeeff43430a78b2e990c972e8bd90473863818d
maye <ssdd050@163.com>
2021-03-18 22:30:23 +0800

    添加扩展函数；逐步替换Java内容

---
+++ app/src/main/java/com/maye/today/expand/KotlinExpand.kt
@@ -0,0 +1,28 @@
+package com.maye.today.expand
+
+import android.view.View
+
+/** View拓展方法 **/
+
+private val viewClickMap = hashMapOf<Int, Long>()
+
+/**
+ * View点击方法
+ */
+fun View.click(clickAction: () -> Unit) {
+    clickAction()
+}
+
+/**
+ * View点击方法（规避快速误触）
+ */
+fun View.safeClick(clickAction: () -> Unit) {
+    val lastClickTime = viewClickMap[id] ?: 0
+    if (System.currentTimeMillis() - lastClickTime > 300) {
+        // 超出误触限制，执行点击逻辑
+        clickAction()
+        viewClickMap.remove(id)
+    } else {
+        viewClickMap[id] = System.currentTimeMillis()
+    }
+}
